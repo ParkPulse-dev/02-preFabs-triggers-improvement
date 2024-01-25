ecce7c816e4747276ad42d8920cec3925a23b604
AvivShevach <92975487+AvivShevach@users.noreply.github.com>
2024-01-25 23:05:02 +0200

    Update DestroyOnTrigger2D.cs

--- Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -1,39 +1,48 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DestroyOnTrigger2D : MonoBehaviour {
+public class DestroyOnTrigger2D : MonoBehaviour
+{
     [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
     [SerializeField] string triggeringTag;
     [SerializeField] NumberField liveText;
     [SerializeField] GameObject spaceship;
-    public int hitPoints;// = 3;
+    public int hitPoints;
 
-    private void Start() {
+    private void Start()
+    {
         hitPoints = 3;
         liveText = GameObject.FindGameObjectWithTag("LiveTag").GetComponent<NumberField>();
         spaceship = GameObject.FindGameObjectWithTag("Player");
-        if (this.gameObject.name == "PlayerSpaceship") {
+        if (this.gameObject.name == "PlayerSpaceship")
+        {
             liveText.SetNumber(hitPoints);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && enabled) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
             Destroy(other.gameObject);
 
-            if (this.gameObject.name == "PlayerSpaceship") {
+            if (this.gameObject.name == "PlayerSpaceship")
+            {
                 hitPoints--;
                 liveText.SetNumber(hitPoints);
-            
 
-                if (hitPoints == 0) {
+
+                if (hitPoints == 0)
+                {
                     Debug.Log("Game over!");
                     Destroy(this.gameObject);
                     Application.Quit();
-                    
+
                 }
-            } else {
+            }
+            else
+            {
                 spaceship.transform.localScale += new Vector3(0.05f, 0.05f, 0);
                 Destroy(this.gameObject);
             }
@@ -41,7 +50,8 @@ public class DestroyOnTrigger2D : MonoBehaviour {
     }
 
 
-    private void Update() {
+    private void Update()
+    {
         /* Just to show the enabled checkbox in Editor */
     }
 }
